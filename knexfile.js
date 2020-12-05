f62853cd9ba2b611d66b8e97a3de1754cde22c23
Carlos Castillo <closcastillo95@gmail.com>
2020-12-05 00:31:30 -0800

    fixing database

--- knexfile.js
+++ knexfile.js
@@ -14,16 +14,15 @@ module.exports = {
 	//   	},
 	// },
 	production: {
-		client: 'pg',
+		client: "pg",
 		connection: process.env.DATABASE_URL,
 		pool: {
-		  min: 2,
-		  max: 10
+			min: 2,
+			max: 10,
 		},
 		migrations: {
-		  tableName: 'knex_migrations',
-		  directory: './migrations'
+			tablename: "knex_migrations",
+			directory: "./migrations",
 		},
-		ssl: false
-	}
+	},
 };
