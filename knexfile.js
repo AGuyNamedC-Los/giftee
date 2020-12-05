9d0fd808c0cb3f3cdf15f2ef9870bc49ec4f9b5c
Carlos Castillo <closcastillo95@gmail.com>
2020-12-04 23:11:57 -0800

    fixing heroku database

--- knexfile.js
+++ knexfile.js
@@ -2,22 +2,30 @@
 
 module.exports = {
 	development: {
-		client: "sqlite3",
-		connection: {
-			filename: "./data/users.sqlite3",
-		},
-		useNullAsDefault: true
+	  	client: "sqlite3",
+	  	useNullAsDefault: true,
+	  	connection: {
+			filename: "./data/users.db",
+	  	},
+	 	pool: {
+			afterCreate: (conn, done) => {
+		  	conn.run("PRAGMA foreign_keys = ON", done);
+			},
+	  	},
 	},
 	production: {
-		client: "pg",
-		connection: process.env.DATABASE_URL,
-		pool: {
-		  min: 2,
-		  max: 10,
+	  	client: "pg",
+	  	connection: {
+			  connectionString: process.env.DATABASE_URL,
+			  ssl: false
 		},
-		migrations: {
-		  tablename: "knex_migrations",
-		  directory: "./migrations",
-		},
-	  },
+	  	pool: {
+			min: 2,
+			max: 10,
+	  	},
+	  	migrations: {
+			tablename: "knex_migrations",
+			directory: "./migrations",
+	  	},
+	},
 };
